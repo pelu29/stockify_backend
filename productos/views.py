c5bee212b1bc1c50de7ab5e04c3f8e58838ad53a
SEBASTIAN <sebastian123abc123abcgmail.com>
2025-10-10 13:08:46 -0500

    Crear vista de endpoint para productos

--- productos/views.py
+++ productos/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class ProductListView(APIView):
+    def get(self, request):
+        productos = [
+            {"nombre": "Café", "categoria": "Bebidas", "precio": 10.5},
+            {"nombre": "Té", "categoria": "Bebidas", "precio": 8.0},
+            {"nombre": "Galleta", "categoria": "Snack", "precio": 2.5},
+        ]
+        return Response(productos)
