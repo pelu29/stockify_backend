1121eaa715eaea79899e63433e3d26be4fbccfbe
Darwin Flores <floresjack74@gmail.com>
2025-10-18 13:57:36 -0500

    Remove ProductListView from URL patterns

--- stockify_backend/urls.py
+++ stockify_backend/urls.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-
-from apps.inventario.views import ProductListView
 from apps.inventario.views import CategoriaViewSet
 
 
@@ -13,6 +11,5 @@ router.register(r'api/categorias', CategoriaViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/products/', ProductListView.as_view()),
     path('', include(router.urls)),
 ]
