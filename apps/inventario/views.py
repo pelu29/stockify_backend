d57e276cc744b0692f8f22b513d5c182ded38083
Darwin Flores <floresjack74@gmail.com>
2025-10-18 13:56:30 -0500

    Remove ProductListView and simulated products
    Removed unused ProductListView and its associated products data.

--- apps/inventario/views.py
+++ apps/inventario/views.py
@@ -7,15 +7,6 @@ from rest_framework import viewsets
 from .models import Categorias
 from .serializers import CategoriaSerializer
 
-from .products_data import productos_simulados
-
-class ProductListView(APIView):
-    def get(self, request):
-       
-        products = productos_simulados
-        
-      
-        return Response(products)
     
 class CategoriaViewSet(viewsets.ModelViewSet):
     queryset = Categorias.objects.all()
