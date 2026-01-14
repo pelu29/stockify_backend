77f376e6e3f6ba14cdcee87952aca022cd4d8a08
TheRick006 <ERICK@DESKTOP>
2026-01-14 11:39:42 -0500

    Agregadas las validaciones para Usuarios

--- apps/usuarios/serializers.py
+++ apps/usuarios/serializers.py
@@ -7,13 +7,28 @@ class ClienteSerializer(serializers.ModelSerializer):
         model = Clientes
         fields = '__all__'
         extra_kwargs = {
-            'password': {'write_only': True}
+            'password': {'write_only': True, 'required': True}
         }
 
     def validate_email(self, value):
         if Clientes.objects.filter(email=value).exists():
             raise serializers.ValidationError("Este correo ya está registrado.")
         return value
+
+    def validate_telefono(self, value):
+        value_str = str(value)
+        digitos= ''.join(c for c in value_str if c.isdigit())
+
+        if len(digitos) != 9:
+            raise serializers.ValidationError("El telefono solo puede tener 9 digitos")
+        
+        if Clientes.objects.filter(telefono=int(value_str)).exists():
+            raise serializers.ValidationError("Este numero ya ha sido registrado")
+        
+        digitos = int(digitos)
+        return digitos
+
+
 
     def create(self, validated_data):
         validated_data['password'] = make_password(validated_data['password'])
