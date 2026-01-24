3c83712adf9db3a6898738848cf7a1d906e879f8
TheRick006 <ERICK@DESKTOP>
2026-01-24 12:28:04 -0500

    Agregada la URL de alertas para el test

--- apps/inventario/urls.py
+++ apps/inventario/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CategoriaViewSet, ProductosViewSet
+from .views import CategoriaViewSet, ProductosViewSet, AlertasViewSet
 
 router = DefaultRouter()
 router.register(r'categorias', CategoriaViewSet, basename='categorias')
 router.register(r'productos', ProductosViewSet, basename='productos')
-
+router.register(r'alertas', AlertasViewSet, basename='alertas')
 
 # ✅ Unimos ambas listas de rutas
 urlpatterns = [
